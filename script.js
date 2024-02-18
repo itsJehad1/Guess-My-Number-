8737bf42c63307cf6c5c70d5599d6bce943d66fd
itsJehad <150825389+itsJehad1@users.noreply.github.com>
2024-02-18 21:16:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+// Generates random number
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When there is no input...
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too Hight' : '📉 Too Low');
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 Game Over!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Attaching click event handler to "Again" button 👆
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.message').textContent = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
